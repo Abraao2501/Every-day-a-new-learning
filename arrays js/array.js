6aef21255899bb17d597952a01f7192dae0c009b
Abraão Oliveira <abraaooliveira.eeq@gmail.com>
2021-01-22 16:42:48 -0300

    Studying array's documentation

---
+++ arrays js/array.js
@@ -0,0 +1,16 @@
+//  --Array it's list-like objects-- //
+
+//Create an array
+var fruits = ['banana','apple','orange']
+
+//Acess an Array item using the index position
+let first = fruits[0]
+let last = fruits.length[-1]
+
+//Add an item to the end of an array
+let newLength = fruits.push('strawberry')
+
+//Remove an item from the end of an array
+let last = fruits.pop()
+
+
